59dd6f8726b875db0c52a2752bcf1a8e44e7c82c
lucass-segura <lucassegura05@gmail.com>
2025-07-28 22:52:31 -0300

    modulo 4

---
+++ modulo_4/lista_busqueda.py
@@ -0,0 +1,19 @@
+# Busqueda en listas
+
+title = ' Curso de Python'
+# strip() quita los espacios en blanco al inicio y al final
+title = title.strip()
+
+print('python' in title.lower())
+print(title.upper())
+
+print(title.count('o'))
+
+print(title.startswith('Curso'))
+print(title.endswith('Python'))
+
+print(title.find('P'))
+print('6'.isnumeric())
+
+print('hola mundo!'.capitalize())
+
---
+++ modulo_4/metodos_string.py
@@ -0,0 +1,12 @@
+# Split
+# Separa una cadena de texto en una lista
+nombres = "Agus, Juan, Pedro, Maria, Luis"
+lista_nombres = nombres.split(",") # ['Agus', ' Juan', ' Pedro', ' Maria', ' Luis']
+print(lista_nombres)
+
+# Join
+# Une una lista de cadenas de texto en una cadena de texto
+lista_nombres = ["Agus", "Juan", "Pedro", "Maria", "Luis"]
+nombres = ", ".join(lista_nombres) # Agus, Juan, Pedro, Maria, Luis
+print(nombres)
+
